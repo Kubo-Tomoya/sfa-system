647ea68ca24c576a4d943c9c9face4620d17e38e
Kubo-Tomoya <t.kubo.cingroup2012@gmail.com>
2026-03-13 01:27:18 +0900

    プロジェクト初期設定

---
+++ src/main/java/com/example/sfasystem/SfasystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.sfasystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfasystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfasystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sfasystem/SfasystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sfasystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfasystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
